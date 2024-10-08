70915e70b4181a3996f58ec69ee82eb6266def56
Praveen <delhicharmohan@gmail.com>
2024-10-08 19:12:39 +0530

    fixing error with user.js

--- models/User.js
+++ models/User.js
@@ -1,11 +1,11 @@
-// models/User.js
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  telegramId: { type: String },
-  email: { type: String }, 
+  telegramId: { type: String, required: true, unique: true }, // Ensure telegramId is unique
+  email: { type: String }, // Remove 'required: true' if email is optional
   firstName: { type: String },
   lastName: { type: String },
 });
 
 module.exports = mongoose.model('User', UserSchema);
+UserSchema.set('autoIndex', true);
