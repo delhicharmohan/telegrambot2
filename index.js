e9a3366a59fffa71efe414472b862f0ccd778958
Praveen <delhicharmohan@gmail.com>
2024-10-08 21:47:44 +0530

    render port error

--- index.js
+++ index.js
@@ -716,7 +716,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0' ,() => {
   console.log(`Server running on port ${PORT}`);
 });
